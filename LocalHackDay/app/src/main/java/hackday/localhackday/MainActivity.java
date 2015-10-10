7d0cb4cd9fc5c257ee2f2ddd4b550bb80e5a59a2
Gabriel Lopez de Leon <lopezdg@mcmaster.ca>
2015-10-10 16:10:33 -0400

    Addition of Java Files From Android Studio

---
+++ LocalHackDay/app/src/main/java/hackday/localhackday/MainActivity.java
@@ -0,0 +1,56 @@
+package hackday.localhackday;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    //button listeners go in 'onCreate'
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton bSettings = (ImageButton)findViewById(R.id.bSettings);
+
+        bSettings.setOnClickListener(
+                new ImageButton.OnClickListener(){
+                    public void onClick(View v){
+                        Intent goToSettings = new Intent(v.getContext(), settingsActivity.class);
+                        startActivityForResult(goToSettings, 0);
+                    }
+                }
+        );
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
